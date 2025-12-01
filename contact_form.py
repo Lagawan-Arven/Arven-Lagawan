1fa523fce0dff7442b6b9e3945dcd426ac3e83ee
Arven Lagawan <arvenlagawan0731@gmail.com>
2025-12-01 15:56:45 +0800

    Add files via upload

---
+++ contact_form.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import psycopg2
+
+app = FastAPI()
+
+class Contact(BaseModel):
+    name: str
+    email: str
+    message: str
+
+def get_conn():
+    return psycopg2.connect(
+        dbname="mydb",
+        user="postgres",
+        password="Charren30!",
+        host="localhost",
+        port="5432"
+    )
+
+@app.post("/index.html")
+def contact_form(data: Contact):
+    conn = get_conn()
+    cur = conn.cursor()
+
+    cur.execute("""
+        INSERT INTO contact_messages (name, email, message)
+        VALUES (%s, %s, %s);
+    """, (data.name, data.email, data.message))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    return {"status": "success", "msg": "Message saved!"}
