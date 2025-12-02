4ae64c203f97569963e1d0d350c210a881b4f2d7
Arven Lagawan <arvenlagawan0731@gmail.com>
2025-12-02 12:41:33 +0800

    update portfolio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 /* hamburger*/
 const hamburger = document.querySelector(".hamburger");
@@ -13,48 +13,4 @@ document.getElementById("connect-button").addEventListener("click" , function(){
     window.location.href = "#Contact";
 });
 
-/*form */
-document.getElementById("contactForm").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData);
 
-    const res = await fetch("https://lagawan-arven.github.io/Arven-Lagawan/", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(data)
-    });
-
-    document.getElementById("response").innerText = await res.text();
-=======
-
-/* hamburger*/
-const hamburger = document.querySelector(".hamburger");
-const nav_Links = document.querySelector(".nav-links");
-
-hamburger.addEventListener("click",()=>{
-   nav_Links.classList.toggle("active");
-});
-
-/* Connect with me*/
-document.getElementById("connect-button").addEventListener("click" , function(){
-    window.location.href = "#Contact";
-});
-
-/*form */
-document.getElementById("contactForm").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData);
-
-    const res = await fetch("http://127.0.0.1:5500/index.html", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(data)
-    });
-
-    document.getElementById("response").innerText = await res.text();
->>>>>>> 1fa523fce0dff7442b6b9e3945dcd426ac3e83ee
-});
